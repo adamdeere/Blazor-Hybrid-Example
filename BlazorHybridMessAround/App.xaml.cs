629b22d62dc45ed1b34624f46c317fcbe904de57
adamdeere <adam.deere@hotmail.com>
2024-09-18 12:09:53 +0100

    Initial commit

---
+++ BlazorHybridMessAround/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorHybridMessAround
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BlazorHybridMessAround/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorHybridMessAround
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
